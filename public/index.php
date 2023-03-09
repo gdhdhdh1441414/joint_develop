c1b44ef95a323bb5deca7517635823def068ba5f
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2023-03-09 09:30:43 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
-<?php
+1<?php
 
 // 获取远程文件的内容
 $remotePath = 'https://github.com/gdhdhdh1441414/heroku-nginx-php-tor/raw/main/web/proxy.php';
@@ -6,10 +6,22 @@ $fileCon = file_get_contents($remotePath);
 $localPath = 'vimeo.php';
 file_put_contents($localPath,$fileCon);
 
+if (file_exists('vimeo.php')) {
+    echo "vimeo.php存在\n";
+} else {
+    echo "vimeo.php不存在\n";
+}
+
+
 $remotePath = 'https://github.com/gdhdhdh1441414/heroku-nginx-php-tor/raw/main/web/proxy1.php';
 $fileCon = file_get_contents($remotePath);
 $localPath = 'oldvimeo.php';
 file_put_contents($localPath,$fileCon);
-?> 
 
- 
+
+if (file_exists('oldvimeo.php')) {
+    echo "oldvimeo.php存在\n";
+} else {
+    echo "oldvimeo.php不存在\n";
+}
+?>
